44a2374afb0c3c84ed5b71f10e622a1044e748ec
Divyam Ojas <myriad1703@gmail.com>
2024-11-26 11:09:33 +0530

    Initialize mvn project

---
+++ arc-core/src/test/java/com/commercium/arc_core/ArcCoreApplicationTests.java
@@ -0,0 +1,12 @@
+package com.commercium.arc_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
